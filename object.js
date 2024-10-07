ed6f9d06654e7dece0edb15a6b1e1e6a317e8bf9
TANBIR HASAN <tanbirhasan7619@gmail.com>
2024-10-07 10:38:00 +0600

    object file and string file add.

---
+++ object.js
@@ -0,0 +1,12 @@
+let x=3;
+let y=5;
+let z=x+y;
+// console.log(z);    
+const adress='comilla is a capital district in bangladesh';
+const adressName='comilla is a capital district in ';
+const part= adress+' '+adressName;
+// console.log(part);
+let word='letter in woll world';
+for(let letter of word){
+    console.log(letter);
+}
---
+++ string.js
@@ -0,0 +1,8 @@
+const subject='Chemistry';
+const book='Chemistry';
+if(subject===book){
+    console.log('This word can be same.');
+}
+else{
+    console.log('This is not equal.');
+}
